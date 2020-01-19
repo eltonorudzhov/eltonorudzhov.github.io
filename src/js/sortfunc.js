10bcff9924f98fd429313e6f56f91b67c38f089b
eltonorudzhov <56114594+eltonorudzhov@users.noreply.github.com>
2020-01-19 21:04:53 +0300

    tensorTest1

--- src/js/sortfunc.js
+++ src/js/sortfunc.js
@@ -1,20 +1,26 @@
 import drowList from "./drowlist.js"; // рисует
 import makeObject from "./makeobject.js"; // мапит массив
 import findProperty from "./findproperty.js"; // ищет радиобаттон
+import mouseEvent from './mouseevent.js'
 export default class Sortfunc {
   constructor(props, root) {
-    this.root=root;
-    this.numberElement=root.numberElement;
-    this.root.createRoot(['name','secondname', 'age'])
-    let form = document.getElementById('form'+this.numberElement);
+    this.root = root;
+    this.numberElement = root.numberElement;
+    let propsKeys = [];
+    for (let keys in props[0]) {
+      propsKeys.push(keys);
+    }
+    this.root.createRoot(propsKeys);
+    let form = document.getElementById("form" + this.numberElement);
     // берем параметр сортировки
     this.keyName = findProperty(this.numberElement);
     this.list = [];
     // создаем и сортируем список
-    
-    this.listBody =  document.getElementById('bodyList'+this.numberElement)
-    this.listBody.innerHTML=''
+    this.listBody = document.getElementById("bodyList" + this.numberElement);
+    this.listBody.innerHTML = "";
     this.setList(props);
+    // событие обработки касания мыши
+    mouseEvent(this.listBody)
     // обработчик выбора способа сортировки
     form.addEventListener(
       "submit",
@@ -24,54 +30,9 @@ export default class Sortfunc {
       },
       false
     );
-  /*  this.listBody.addEventListener('click', ()=> {
-      this.listBody.children.scrollTop = 10;
-})*/
-  /*
-    this.listBody.onmousedown = (event)=> {
-
-     // let shiftX = event.clientX - this.listBody.getBoundingClientRect().left;
-      let shiftY = event.clientY - this.listBody.getBoundingClientRect().top;
-      
-      this.listBody.style.position = 'absolute';
-      this.listBody.style.zIndex = 1000;
-      document.body.append(this.listBody);
-    
-     // moveAt(event.pageY, this.listBody).bind(this);
-     this.listBody.style.top = event.pageY - shiftY + 'px';
-      // переносит мяч на координаты (pageX, pageY),
-      // дополнительно учитывая изначальный сдвиг относительно указателя мыши
-      function moveAt( pageY,listBody) {
-        console.log("SSS")
-       // this.listBody.style.left = pageX - shiftX + 'px';
-        listBody.style.top = pageY - shiftY + 'px';
-      }
-      
-      function onMouseMove(event) {
-       // moveAt( event.pageY).bind(this);
-       this.listBody.style.top = event.pageY - shiftY + 'px';
-      }
-    
-      // передвигаем мяч при событии mousemove
-      document.addEventListener('mousemove', onMouseMove.bind(this));
-    
-      // отпустить мяч, удалить ненужные обработчики
-      this.listBody.onmouseup =()=> {
-        document.removeEventListener('mousemove', onMouseMove.bind(this));
-        this.listBody.onmouseup = null;
-        
-        return;
-      };
-    
-    };
-    
-    this.listBody.ondragstart = ()=> {
-      return false;
-    };
-*/   
   }
   // Выбор способа сортировки
-  sortSelect = (form) => {
+  sortSelect = form => {
     let data = new FormData(form);
     for (const entry of data) {
       this.keyName = entry[1];
@@ -82,16 +43,12 @@ export default class Sortfunc {
   setList(list) {
     this.list = list.sort(this.compareList);
     let mapList = makeObject(this.list, this.keyName);
-    this.listBody=drowList(mapList,this.keyName, this.numberElement)
+    this.listBody = drowList(mapList, this.keyName, this.numberElement);
   }
-  
+
   compareList = (a, b) => {
     if (a[this.keyName] > b[this.keyName]) return 1;
     if (a[this.keyName] == b[this.keyName]) return 0;
     if (a[this.keyName] < b[this.keyName]) return -1;
   };
-
-
-
 }
-
